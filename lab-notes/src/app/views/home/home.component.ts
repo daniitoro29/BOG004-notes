4d3da520024aaa027038a2b20e2876457016618d
daniitoro29 <daniitoro29@gmail.com>
2022-05-27 11:42:35 -0500

    Enlazamos el proyecto con firebase y materialize close #5

--- lab-notes/src/app/views/home/home.component.ts
+++ lab-notes/src/app/views/home/home.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+img: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  //Esto es un método. Solo se activa cuando le damos click al botón
+  showTag(){
+    this.img=true;
   }
 
 }
